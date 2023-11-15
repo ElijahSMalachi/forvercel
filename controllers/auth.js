30054811aeac7cca13eb44b74f6c828bf67771a6
Elijah S Malachi <ElijahSMalachi@gmail.com>
2023-11-15 00:10:00 -0800

    with cons

---
+++ controllers/auth.js
@@ -0,0 +1,80 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+  
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const isUser = await User.findOne({ email: email.toLowerCase() })
+    if(isUser){
+        const isMatch = await bcrypt.compare(password, isUser.password);
+        if(isMatch){
+          const token = jwt.sign({ id: isUser._id }, process.env.JWT_SECRET);
+          const userWithoutPassword = { ...isUser.toObject() };
+          delete userWithoutPassword.password;
+          res.status(200).json({ token, savedUser:userWithoutPassword });
+
+        }else{
+          res.status(409).json({ emailInUse: 'email is already in use' });
+        
+        }
+    }else{
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+      const newUser = new User({
+        firstName: capitalizeFirstLetter(firstName),
+        lastName: capitalizeFirstLetter(lastName),
+        email: email.toLowerCase(),
+        password: passwordHash,
+        picturePath,
+        friends,
+        location,
+        occupation,
+        viewedProfile: Math.floor(Math.random() * 10000),
+        impressions: Math.floor(Math.random() * 10000),
+      });
+      const savedUser = await newUser.save();
+      const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET);
+      const userWithoutPassword = { ...savedUser.toObject() };
+      delete userWithoutPassword.password;
+      res.status(201).json({ token, savedUser: userWithoutPassword});
+    }
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if (!user) return res.status(400).json({ NoUser: "User does not exist" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ IncPass: "Wrong Password. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const userWithoutPassword = { ...user.toObject() };
+    delete userWithoutPassword.password;
+    res.status(200).json({ token, user:userWithoutPassword });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+import express from "express";
 import bodyParser from "body-parser";
-import express from 'express';
 import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import User from "./modals/User.js";
 
-const app = express()
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
 
 app.post('/post', async(req,res)=>{
         const {
@@ -22,7 +53,7 @@ app.post('/post', async(req,res)=>{
         const newUser = new User({
             firstName: firstName,
             lastName: lastName,
-            email: 'ElijahSMalachi@gmail.com',
+            email: email.toLowerCase(),
             password,
             picturePath,
             friends,
