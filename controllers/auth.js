b6cd6a2f12a1fada4d55d6a99f7fecb2fc4a48e1
Elijah S Malachi <121290719+ElijahSMalachi@users.noreply.github.com>
2023-11-15 00:32:14 -0800

    heylo

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,60 +4,60 @@ import User from "../models/User.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => {
-  
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-  try {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      picturePath,
-      friends,
-      location,
-      occupation,
-    } = req.body;
+  res.status(200).json('Heylo from register');
+// function capitalizeFirstLetter(string) {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+//   try {
+//     const {
+//       firstName,
+//       lastName,
+//       email,
+//       password,
+//       picturePath,
+//       friends,
+//       location,
+//       occupation,
+//     } = req.body;
 
-    const isUser = await User.findOne({ email: email.toLowerCase() })
-    if(isUser){
-        const isMatch = await bcrypt.compare(password, isUser.password);
-        if(isMatch){
-          const token = jwt.sign({ id: isUser._id }, process.env.JWT_SECRET);
-          const userWithoutPassword = { ...isUser.toObject() };
-          delete userWithoutPassword.password;
-          res.status(200).json({ token, savedUser:userWithoutPassword });
+//     const isUser = await User.findOne({ email: email.toLowerCase() })
+//     if(isUser){
+//         const isMatch = await bcrypt.compare(password, isUser.password);
+//         if(isMatch){
+//           const token = jwt.sign({ id: isUser._id }, process.env.JWT_SECRET);
+//           const userWithoutPassword = { ...isUser.toObject() };
+//           delete userWithoutPassword.password;
+//           res.status(200).json({ token, savedUser:userWithoutPassword });
 
-        }else{
-          res.status(409).json({ emailInUse: 'email is already in use' });
+//         }else{
+//           res.status(409).json({ emailInUse: 'email is already in use' });
         
-        }
-    }else{
-      const salt = await bcrypt.genSalt();
-      const passwordHash = await bcrypt.hash(password, salt);
-      const newUser = new User({
-        firstName: capitalizeFirstLetter(firstName),
-        lastName: capitalizeFirstLetter(lastName),
-        email: email.toLowerCase(),
-        password: passwordHash,
-        picturePath,
-        friends,
-        location,
-        occupation,
-        viewedProfile: Math.floor(Math.random() * 10000),
-        impressions: Math.floor(Math.random() * 10000),
-      });
-      const savedUser = await newUser.save();
-      const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET);
-      const userWithoutPassword = { ...savedUser.toObject() };
-      delete userWithoutPassword.password;
-      res.status(201).json({ token, savedUser: userWithoutPassword});
-    }
+//         }
+//     }else{
+//       const salt = await bcrypt.genSalt();
+//       const passwordHash = await bcrypt.hash(password, salt);
+//       const newUser = new User({
+//         firstName: capitalizeFirstLetter(firstName),
+//         lastName: capitalizeFirstLetter(lastName),
+//         email: email.toLowerCase(),
+//         password: passwordHash,
+//         picturePath,
+//         friends,
+//         location,
+//         occupation,
+//         viewedProfile: Math.floor(Math.random() * 10000),
+//         impressions: Math.floor(Math.random() * 10000),
+//       });
+//       const savedUser = await newUser.save();
+//       const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET);
+//       const userWithoutPassword = { ...savedUser.toObject() };
+//       delete userWithoutPassword.password;
+//       res.status(201).json({ token, savedUser: userWithoutPassword});
+//     }
 
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
 };
 
 /* LOGGING IN */
